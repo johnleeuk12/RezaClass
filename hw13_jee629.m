a53da184d300e05910a09358ddbcb6ee3c0627b1
johnleeuk12 <johnleeuk12@gmail.com>
2018-03-27 18:41:36 -0400

    S

--- hw13_jee629.m
+++ hw13_jee629.m
@@ -5,9 +5,16 @@ x = 0:0.1:300;
 px_bass = 0.6*pdf('Normal',x,165,7)+0.4*pdf('Normal',x,180,7);
 px_salm = 0.6*pdf('Normal',x,180,3)+0.4*pdf('Normal',x,160,8);
 
+figure
 plot(x,px_bass)
 hold on
 plot(x,px_salm)
+axis([130, 210, 0, inf]);
+xlabel('luminosity')
+title('Likelihood')
+legend('Bass','Salmon')
+
+
 
 % marginal distribution
 px = px_bass*0.4+px_salm*0.6;
@@ -15,6 +22,10 @@ px = px_bass*0.4+px_salm*0.6;
 figure
 plot(x,px)
 axis([130, 210, 0, inf]);
+xlabel('luminosity')
+title('Marginal distribution')
+% legend('Bass','Salmon')
+
 
 % posterior probabilities for each fish species
 pc_bass = px_bass*0.4./px;
@@ -25,3 +36,18 @@ figure
 plot(x,pc_bass)
 hold on
 plot(x,pc_salm)
+axis([130, 210, 0, inf]);
+xlabel('luminosity')
+title('posterior probabilities')
+legend('Bass','Salmon')
+
+
+%Calculating the variance of the posterior probability distribution
+
+var = pc_bass.*pc_salm;
+
+figure
+plot(x,var)
+axis([130, 210, 0, inf]);
+xlabel('luminosity')
+title('Variance of distribution')
