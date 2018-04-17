e6b44dc71254adef91e4ffc35d751e7b373d6fe8
John <johnleeuk21@live.co.kr>
2018-04-16 21:11:18 -0400

    Merge

--- hw14_jlee629.m
+++ hw14_jlee629.m
@@ -1,35 +1,168 @@
 function hw14_jlee629()
 
-
 clear all
-data = load('classification.dat');
 
+Data = load('classification.dat');
 
+%Calculate the mean and variance of each class
 Variance = zeros(2,2);
 for cls = 1:3
-    % Calculate the number of data for each class
-    N(1,cls) = length(find(data(:,1) == cls));
+    N(cls) = length(find(Data(:,1) == cls));
     
-    % Calculate the mean
-    mu(cls,:) = [sum(data(find(data(:,1) == cls),2)) sum(data(find(data(:,1) == cls),3))];
-    mu(cls,:) = mu(cls,:)/N(1,cls);
+    mu(cls,:) = mean(Data(find(Data(:,1) == cls),2:3));
     
-    % Calculate the standard deviation.
     Sigma{cls} = zeros(2,2);
-    for n = find(data(:,1) == cls)
-        Sigma{cls} = Sigma{cls} + (data(n,2:3)-mu(cls,:)).'*(data(n,2:3)-mu(cls,:));
+    for n = find(Data(:,1) == cls)
+        Sigma{cls} = Sigma{cls}+(Data(n,2:3)-mu(cls,:)).'*(Data(n,2:3)-mu(cls,:));
     end
-    Sigma{cls} = Sigma{cls}/N(1,cls);    
-    
-    %Calculate the single variance
-    Variance = Variance + N(1,cls)*Sigma{cls};
+    Variance = Variance+Sigma{cls};
+    Sigma{cls} = Sigma{cls}/N(cls);
+end    
+Variance = Variance/length(Data);
+
+
+% Take a number of points in the data space, and classify each point.
+vec = -10:0.1:10;
+PClass = zeros(length(vec),length(vec));
+
+for n1 = 1: length(vec)
+    for n2 = 1: length(vec)
+        xx1 = vec(n1);
+        xx2 = vec(n2);
+        P(1) = 1/sqrt(norm(Variance))*exp(-.5*(([xx1,xx2]-mu(1,:))*inv(Variance)*([xx1,xx2]-mu(1,:)).'));
+        P(2) = 1/sqrt(norm(Variance))*exp(-.5*(([xx1,xx2]-mu(2,:))*inv(Variance)*([xx1,xx2]-mu(2,:)).'));    
+        P(3) = 1/sqrt(norm(Variance))*exp(-.5*(([xx1,xx2]-mu(3,:))*inv(Variance)*([xx1,xx2]-mu(3,:)).'));
+        PClass(n1,n2) = find(P==max(P))*0.2+0.5;
+    end
+end
+figure
+imagesc(vec,vec,PClass.')
+axis([-8,2,-8,6]);
+colormap(gray)
+caxis([0 1])
+set(gca,'YDir','normal')
+hold on
+
+
+% Calculate the boundary between each class
+
+W(1,:) = mu(1,:)*inv(Variance)-mu(2,:)*inv(Variance);
+W(2,:) = mu(2,:)*inv(Variance)-mu(3,:)*inv(Variance);
+W(3,:) = mu(3,:)*inv(Variance)-mu(1,:)*inv(Variance);
+Wo(1) = 0.5*(-mu(1,:)*inv(Variance)*mu(1,:).' + mu(2,:)*inv(Variance)*mu(2,:).');
+Wo(2) = 0.5*(-mu(2,:)*inv(Variance)*mu(2,:).' + mu(3,:)*inv(Variance)*mu(3,:).');   
+Wo(3) = 0.5*(-mu(3,:)*inv(Variance)*mu(3,:).' + mu(1,:)*inv(Variance)*mu(1,:).');  
+
+x2 = -10:0.5:10;
+for cls = 1:3
+    x1{cls} = -Wo(cls)/W(cls,2)-W(cls,1)/W(cls,2)*x2;
+    scatter(Data(find(Data(:,1) == cls),2),Data(find(Data(:,1) == cls),3),20,'filled')
+    hold on
+    plot(x2,x1{cls});
+    axis([-8,2,-8,6]);
 end
 
-Variance = Variance/sum(N);
+hold off
 
-X = data(:,2:3);
 
-W = Variance*mu(1,:).' + Variance*mu(2,:).' ;
-W0 = -0.5*mu(1,:)*Variance*mu(1,:).'+-0.5*mu(2,:)*Variance*mu(2,:).';
+%% Quadratic
 
-Y = X*W+W0;
+WWq{1}= -0.5*(inv(Sigma{1})-inv(Sigma{2}));   
+WWq{2}= -0.5*(inv(Sigma{2})-inv(Sigma{3}));      
+WWq{3}= -0.5*(inv(Sigma{3})-inv(Sigma{1}));       
+
+Wq(1,:) = mu(1,:)*inv(Sigma{1})-mu(2,:)*inv(Sigma{2});
+Wq(2,:) = mu(2,:)*inv(Sigma{2})-mu(3,:)*inv(Sigma{3});
+Wq(3,:) = mu(3,:)*inv(Sigma{3})-mu(1,:)*inv(Sigma{1});
+
+Woq(1) = 0.5*(-mu(1,:)*inv(Sigma{1})*mu(1,:).' + mu(2,:)*inv(Sigma{2})*mu(2,:).') -0.5*log(norm(Sigma{1})/norm(Sigma{2}));
+Woq(2) = 0.5*(-mu(2,:)*inv(Sigma{2})*mu(2,:).' + mu(3,:)*inv(Sigma{3})*mu(3,:).') -0.5*log(norm(Sigma{2})/norm(Sigma{3}));
+Woq(3) = 0.5*(-mu(3,:)*inv(Sigma{3})*mu(3,:).' + mu(1,:)*inv(Sigma{1})*mu(1,:).') -0.5*log(norm(Sigma{3})/norm(Sigma{1}));    
+
+
+syms x1 x2
+
+eqn{1} = [x1 x2]*WWq{1}*[x1 x2].' + Wq(1,:)*[x1 x2].' + Woq(1);
+eqn{2} = [x1 x2]*WWq{2}*[x1 x2].' + Wq(2,:)*[x1 x2].' + Woq(2);
+eqn{3} = [x1 x2]*WWq{3}*[x1 x2].' + Wq(3,:)*[x1 x2].' + Woq(3);
+
+PclassQuad = zeros(length(vec),length(vec));
+for n1 = 1: length(vec)
+    for n2 = 1: length(vec)
+        xx1 = vec(n1);
+        xx2 = vec(n2);
+        P(1) = 1/sqrt(norm(Sigma{1}))*exp(-.5*(([xx1,xx2]-mu(1,:))*inv(Sigma{1})*([xx1,xx2]-mu(1,:)).'));
+        P(2) = 1/sqrt(norm(Sigma{2}))*exp(-.5*(([xx1,xx2]-mu(2,:))*inv(Sigma{2})*([xx1,xx2]-mu(2,:)).'));    
+        P(3) = 1/sqrt(norm(Sigma{3}))*exp(-.5*(([xx1,xx2]-mu(3,:))*inv(Sigma{3})*([xx1,xx2]-mu(3,:)).'));
+        PClassQuad(n1,n2) = find(P==max(P))*0.2+0.5;
+    end
+end
+
+
+figure
+imagesc(vec,vec,PClassQuad.')
+axis([-8,2,-8,6]);
+colormap(gray)
+caxis([0 1])
+hold on
+set(gca,'YDir','normal')
+
+for cls = 1:3
+    scatter(Data(find(Data(:,1) == cls),2),Data(find(Data(:,1) == cls),3),20,'filled')
+    hold on
+    fimplicit(eqn{cls})
+    axis([-8,2,-8,6]);
+end   
+    
+    
+%% Gaussian Kernel density estimation 
+
+h = .5;
+PclassGauss = zeros(length(vec),length(vec));
+
+for n1 = 1: length(vec)
+    for n2 = 1: length(vec)
+        xx1 = vec(n1);
+        xx2 = vec(n2);
+        P = zeros(1,3);
+        for cls = 1:3
+            ind = find(Data(:,1) == cls);
+            for nn = 1:length(ind)
+                u = ([xx1 xx2]-Data(ind(nn),2:3))/h;
+                P(cls) = P(cls)+1/(2*pi)*exp(-0.5*(u*u.'));
+            end
+        end
+%         P(2) = 1/sqrt(norm(Sigma{2}))*exp(-.5*(([xx1,xx2]-mu(2,:))*inv(Sigma{2})*([xx1,xx2]-mu(2,:)).'));    
+%         P(3) = 1/sqrt(norm(Sigma{3}))*exp(-.5*(([xx1,xx2]-mu(3,:))*inv(Sigma{3})*([xx1,xx2]-mu(3,:)).'));
+        PclassGauss(n1,n2) = find(P==max(P))*0.2+0.5;
+    end
+end
+
+ 
+figure
+imagesc(vec,vec,PclassGauss.')
+axis([-8,2,-8,6]);
+colormap(gray)
+caxis([0 1])
+hold on
+set(gca,'YDir','normal')
+
+for cls = 1:3
+    scatter(Data(find(Data(:,1) == cls),2),Data(find(Data(:,1) == cls),3),20,'filled')
+    hold on
+    axis([-8,2,-8,6]);
+end   
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- hw16_jlee629.m
+++ hw16_jlee629.m
@@ -1,29 +1,34 @@
 function hw16_jlee629()
 
+clear all
 % Inputs
 
 X = [[0 1 0 1]' [0 0 1 1]'];
 y = [0 1 1 0]';
 
-param = rand(1,7);
+param = rand(1,7)*10;
 sigmo = @(x) 1./(1+exp(-x));
-eta = 0.2;
+eta = 2;
 
 %estimating parameters
+for trial = 1:100000
+    var1 = param(4)*X(:,1) + param(5)*X(:,2)+param(7);
+    var2 = param(1)*X(:,1) + param(2)*X(:,2)+param(6) + param(3)*sigmo(var1);
+    yhat = sigmo(var2);
+    Err = y-yhat;
+    LMS(trial) = immse(y,yhat);
+    Dparam = zeros(1,7);
+    for p = 1:4
+        Dparam(1) = Dparam(1)+ Err(p)*yhat(p)*(1-yhat(p))*X(p,1);
+        Dparam(2) = Dparam(2)+ Err(p)*yhat(p)*(1-yhat(p))*X(p,2);
+        Dparam(3) = Dparam(3)+ Err(p)*yhat(p)*(1-yhat(p))*sigmo(var1(p));
+        Dparam(4) = Dparam(4)+ Err(p)*yhat(p)*(1-yhat(p))*param(3)*sigmo(var1(p))*(1-sigmo(var1(p)))*X(p,1);
+        Dparam(5) = Dparam(5)+ Err(p)*yhat(p)*(1-yhat(p))*param(3)*sigmo(var1(p))*(1-sigmo(var1(p)))*X(p,2);
+        Dparam(6) = Dparam(6)+ Err(p)*yhat(p)*(1-yhat(p));
+        Dparam(7) = Dparam(7)+ Err(p)*yhat(p)*(1-yhat(p))*param(3)*sigmo(var1(p))*(1-sigmo(var1(p)));
+    end
+    param = param + eta*Dparam;
+end
+    
 
-var1 = param(4)*X(:,1)+param(6) + param(5)*X(:,2)+param(6);
-var2 = param(1)*X(:,1)+param(6) + param(2)*X(:,2)+param(6) + param(3)*sigmo(var1);
-yhat = sigmo(var2);
-Err = y-yhat;
-Dparam = zeros(1,7);
-for p = 1:4
-    Dparam(1) = Dparam(1)+ Err(p)*yhat(1-yhat)*X(p,1);
-    Dparam(2) = Dparam(2)+ Err(p)*yhat(1-yhat)*X(p,2);
-    Dparam(3) = Dparam(3)+ Err(p)*yhat(1-yhat)*sigmo(var1(p));
-    Dparam(4) = Dparam(4)+ Err(p)*yhat(1-yhat)*
-    
-    
-    http://bengio.abracadoudou.com/lectures/mlp.pdf     
-    http://bengio.abracadoudou.com/lectures/old/tex_ann.pdf
-    
-
+plot(LMS)
